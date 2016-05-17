154444cabb24eda03ec8cf0de65ec04b6d49f816
JanRK <jk@sys-adm.dk>
2016-05-17 09:40:07 +0200

    Create WinClean.bat

---
+++ WinClean.bat
@@ -0,0 +1,33 @@
+Rem All version of windows
+net stop bits
+net stop wuauserv
+
+del "%windir%\SoftwareDistribution\Download\*" /F /Q
+for /d %%i in ("%windir%\SoftwareDistribution\Download\*") do rmdir /s /q "%%i" 
+
+del "C:\Windows\ccmcache\*" /F /Q
+for /d %%i in ("C:\Windows\ccmcache\*") do rmdir /s /q "%%i" 
+
+del "%windir%\temp\*" /F /Q
+for /d %%i in ("%windir%\temp\*") do rmdir /s /q "%%i" 
+
+net start bits
+net start wuauserv
+compact /c /s:"%windir%\system32\LogFiles"
+
+Rem 2008 and up
+
+for /d %%i in ("c:\$Recycle.Bin\*") do rmdir /s /q "%%i" 
+
+for /d %%i in ("c:\programdata\microsoft\windows\wer\reportqueue\*") do rmdir /s /q "%%i" 
+
+dism /online /cleanup-image /spsupserceded
+dism /online /cleanup-image /startcomponentcleanup
+
+IF EXIST "C:\Users\" (
+ for /D %%x in ("C:\Users\*") do (               
+  forfiles /p "%%x\AppData\Local\Temp" /s /m *.* /D -7 /C "cmd /c del /Q @path"
+  forfiles /p "%%x\AppData\Local\Microsoft\Windows\Temporary Internet Files" /s /m *.* /D -7 /C "cmd /c del /Q @path"
+  forfiles /p "%%x\AppData\Local\Microsoft\Windows\WER\ReportQueue" /s /m *.* /C "cmd /c del /Q @path"
+ )
+)
