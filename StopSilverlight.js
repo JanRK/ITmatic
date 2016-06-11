3f436db09741af1a729475fa2882f3963c19160e
JanRK <jk@sys-adm.dk>
2016-06-11 20:15:00 +0200

    Create StopSilverlight.js

---
+++ StopSilverlight.js
@@ -0,0 +1,20 @@
+// By Tony Marques
+
+var wu_Session = WScript.CreateObject("Microsoft.Update.Session");
+var wu_Searcher = wu_Session.CreateUpdateSearcher();
+wu_Searcher.Online = false;
+
+do {
+  WScript.echo("Searching...");
+  var silverstreak = false;
+  var searchResult = wu_Searcher.Search("IsHidden=0 And IsInstalled=0");
+
+  for(var i=0; i<searchResult.Updates.Count; i++){
+    var wupdate = searchResult.Updates.Item(i);
+    if ( ! wupdate.Title.indexOf("Microsoft Silverlight") ) { // if -1
+    wupdate.IsHidden=1;
+    WScript.echo("   Hiding update: " + wupdate.Title);
+    silverstreak = true;
+    } else { WScript.echo(".............. " + wupdate.Title);"}
+  }
+} while ( silverstreak );  // repeat search
