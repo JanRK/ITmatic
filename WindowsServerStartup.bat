1757cdb801b3e2fa3d170b67a50a1fb994736c34
JanRK <jk@itmatic.dk>
2019-10-25 14:05:18 +0200

    Update WindowsServerStartup.bat

--- WindowsServerStartup.bat
+++ WindowsServerStartup.bat
@@ -1,2 +1,3 @@
 echo test > c:\test.txt
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/JanRK/ITmatic/master/WindowsServerStartup.ps1'))"
+copy c:\test2.txt c:\test3.txt
