3f79e368273458859fbe953b55c8c7ece37019c8
JanRK <jk@itmatic.dk>
2019-10-25 14:04:40 +0200

    Run pwsh

--- WindowsServerStartup.bat
+++ WindowsServerStartup.bat
@@ -1,2 +1,2 @@
 echo test > c:\test.txt
-echo "test" | Out-File c:\test2.txt
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/JanRK/ITmatic/master/WindowsServerStartup.ps1'))"
